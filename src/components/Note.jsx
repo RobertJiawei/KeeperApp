1405fb60885cd0a4a7f0f73f90d0cba96412c260
Jiawei Wu <robertwu1996@gmail.com>
2023-09-21 17:28:46 -0400

    page basic layout commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Put title here</h1>
+      <p>Put content here</p>
+    </div>
+  );
+}
+
+export default Note;
